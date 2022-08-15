ca9f8e2b4cb88a2015826197ba7a6af8a217f76c
dstuden <studen.david@gmail.com>
2022-08-15 04:14:20 +0200

    netlify

--- src/views/FunView.vue
+++ src/views/FunView.vue
@@ -7,27 +7,31 @@
         <h3>Favorite shows, manga, etc.</h3>
         <ul>
           <li>
-            <img src="@/assets/Images/JJK.webp" class="media" /><a
-              href="https://anilist.co/manga/101517/Jujutsu-Kaisen/"
-              >JuJutsu Kaisen</a
+            <a href="https://anilist.co/manga/101517/Jujutsu-Kaisen/">
+              <img src="@/assets/Images/JJK.webp" class="media" />JuJutsu
+              Kaisen</a
             >
           </li>
           <li>
-            <img src="@/assets/Images/Kaguya.webp" class="media" /><a
-              href="https://anilist.co/manga/86635/Kaguyasama-Love-is-War/"
-              >Kaguya-sama: Love is War</a
+            <a href="https://anilist.co/manga/86635/Kaguyasama-Love-is-War/">
+              <img
+                src="@/assets/Images/Kaguya.webp"
+                class="media"
+              />Kaguya-sama: Love is War</a
             >
           </li>
           <li>
-            <img src="@/assets/Images/JoJo.webp" class="media" /><a
+            <a
               href="https://anilist.co/manga/31706/JoJo-no-Kimyou-na-Bouken-Steel-Ball-Run/"
-              >JoJo no Kimyou na Bouken</a
+            >
+              <img src="@/assets/Images/JoJo.webp" class="media" />JoJo no
+              Kimyou na Bouken</a
             >
           </li>
           <li>
-            <img src="@/assets/Images/MrRobot.webp" class="media" /><a
-              href="https://www.imdb.com/title/tt4158110/"
-              >Mr. Robot</a
+            <a href="https://www.imdb.com/title/tt4158110/">
+              <img src="@/assets/Images/MrRobot.webp" class="media" />Mr.
+              Robot</a
             >
           </li>
           <li>
@@ -79,6 +83,19 @@ ul > li > ul > li {
   flex-direction: row;
   column-gap: 15px;
   align-items: center;
+
+  @media screen and (max-width: 400px) {
+    a {
+      flex-direction: column !important;
+      align-items: unset !important;
+    }
+  }
+  a {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 15px;
+  }
 }
 
 .media {
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
     },
   },
   server: {
-    host: true,
+    host: "0.0.0.0",
   },
 });
