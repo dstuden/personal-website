ff604b63088b39141cb8ea944a69b64836f39db1
dstuden <studen.david@gmail.com>
2023-05-28 13:09:31 +0200

    style update

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ fetch("https://catfact.ninja/fact?max_length=100")
   .then((data) => {
     document.querySelector("#cat-content").innerHTML = data.fact;
   });
-
-const copy = (param) => {
-  if (param === "discord") navigator.clipboard.writeText("David&#5419");
-};
