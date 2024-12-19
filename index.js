e4d7d78c2859c6d35e1f05cf57ba26fb773233c7
David Studen <studen.david@gmail.com>
2024-12-19 23:58:40 +0100

    .

--- index.js
+++
@@ -1,5 +0,0 @@
-fetch("https://catfact.ninja/fact?max_length=100")
-  .then((response) => response.json())
-  .then((data) => {
-    document.querySelector("#cat-content").innerHTML = data.fact;
-  });
